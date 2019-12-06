e0d2f9efc67b7a2a01185a5b7d7236b9ccce1081
yuchangjun <yw-jun888@163.com>
2019-12-06 18:27:31 +0800

    1

--- Unity/Assets/FairyGuiExamples/Editor/BuildAssetBundles.cs
+++ Unity/Assets/FairyGuiExamples/Editor/BuildAssetBundles.cs
@@ -5,6 +5,33 @@ using System.IO;
 public class BuildAssetBundles
 {
     [MenuItem("Window/Build FairyGUI Example Bundles")]
+
+
+    public static void Build()
+    {
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    AssetImporter.GetAtPath("Assets/FairyGUI/Examples/Resources/Icons/i" + i + ".png").assetBundleName = "fairygui-examples/i" + i + ".ab";
+        //}
+
+        //AssetImporter.GetAtPath("Assets/FairyGUI/Examples/Resources/UI/BundleUsage_fui.bytes").assetBundleName = "fairygui-examples/bundleusage.ab";
+        //AssetImporter.GetAtPath("Assets/FairyGUI/Examples/Resources/UI/BundleUsage_atlas0.png").assetBundleName = "fairygui-examples/bundleusage.ab";
+
+        string folder = "Assets/UI/";
+        string[] filePaths = Directory.GetFiles(folder);
+        for(int i =0;i<filePaths.Length;++i)
+        {
+            string p = filePaths[i];
+            if(Path.GetExtension(p).EndsWith(".meta"))
+            {
+                continue;
+            }
+            AssetImporter.GetAtPath(p).assetBundleName = Path.GetFileNameWithoutExtension(p);
+        }
+        
+        BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+
     public static void Builde()
     {
 #if (UNITY_5 || UNITY_5_3_OR_NEWER)
