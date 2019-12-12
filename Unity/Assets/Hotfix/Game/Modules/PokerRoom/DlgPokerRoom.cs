b56865891ebe7fa10e34c1f7185d11f25460071e
yuchangjun <yw-jun888@163.com>
2019-12-12 16:59:27 +0800

    测试Poker

--- Unity/Assets/Hotfix/Game/Modules/PokerRoom/DlgPokerRoom.cs
+++ Unity/Assets/Hotfix/Game/Modules/PokerRoom/DlgPokerRoom.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FairyGUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +13,29 @@ namespace ETHotfix
         {
             this.pakName = "Poker";
             this.cmpName = "Cards";
-            this.CurrentUIType.NeedClearingStack = true;
-            this.CurrentUIType.UIForms_ShowMode = UIFormsShowMode.HideOther;
+            this.CurrentUIType.NeedClearingStack = false;
+            this.CurrentUIType.UIForms_ShowMode = UIFormsShowMode.ReverseChange;
             this.CurrentUIType.UIForms_Type = UIFormsType.Normal;
             Log.Debug("DlgPokerRoom Open");
         }
+
+        private GObject btn_Close;
+        public override void ExportFields()
+        {
+            base.ExportFields();
+            btn_Close = this.GetChild("frame").asCom.GetChild("closeButton");
+        }
+
+        public override void RegistUIEvents()
+        {
+            base.RegistUIEvents();
+            btn_Close.onClick.Add(OnButton_Close);
+        }
+
+        private void OnButton_Close()
+        {
+            this.Close();
+        }
+
     }
 }
